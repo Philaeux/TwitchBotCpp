07b9ee914b5011ed2cc578f3e02ff9e13fb740c6
Vincent 'Philaeux' Lamotte <philaeux@the-cluster.org>
2020-07-16 21:08:48 +0200

    Starting compilation pipeline

--- TwitchBotCpp/TwitchBotCpp.cpp
+++ TwitchBotCpp/TwitchBotCpp.cpp
@@ -1,9 +1,29 @@
 ﻿#include "TwitchBotCpp.h"
 
+#include <boost/program_options.hpp>
+using namespace boost::program_options;
+
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	cout << "Hello CMake." << endl;
+	// Parse parameters
+	string cfg = "default.cfg";
+	options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "print usage message")
+		("cfg", value<string>()->default_value(cfg), "configuration file to use");
+
+	variables_map vm;
+	store(parse_command_line(argc, argv, desc), vm);
+	
+
+	cout << cfg << endl;
+
+	if (vm.count("help")) {  
+		cout << desc << "\n";
+		return 0;
+	}
+
 	return 0;
 }
